4110315fbcbc7dd84643b130c470814e55898bee
XanderSamGalapia <157377381+XanderSamGalapia@users.noreply.github.com>
2025-05-23 05:42:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import streamlit as st
 from PIL import Image
-import numpy as np
 from ultralytics import YOLO
+import numpy as np
 
 @st.cache_resource
 def load_model():
-    model = YOLO('trained_yolo11l_model20.pt')  
-    return model
+    return YOLO('trained_yolo11l_model20.pt')
 
 def main():
     st.title("Cheating or Normal")
 
     model = load_model()
 
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Upload an Image :)", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_container_width=True)
         results = model(image)
         annotated_frame = results[0].plot()
-        st.image(annotated_frame, caption="Detection Results", use_column_width=True)
+        st.image(annotated_frame, caption="Detection Results", use_container_width=True)
 
 if __name__ == "__main__":
     main()
