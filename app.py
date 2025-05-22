6738d6ebf2cae3032e65f79f6f7949df0dcedb3c
XanderSamGalapia <157377381+XanderSamGalapia@users.noreply.github.com>
2025-05-23 05:22:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
         results = model(image)
         annotated_frame = results[0].plot()
         st.image(annotated_frame, caption="Detection Results", use_column_width=True)
